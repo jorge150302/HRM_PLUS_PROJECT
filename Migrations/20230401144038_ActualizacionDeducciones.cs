55565ec23ddfbd20053d7e2462b8674530e2051b
Jorge Almanzar <jorgeluisalmanzar@gmail.com>
2023-04-02 00:19:03 -0400

    Agregando las validaciones de tipodeduccion

---
+++ Migrations/20230401144038_ActualizacionDeducciones.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRM_PLUS_PROJECT.Migrations
+{
+    public partial class ActualizacionDeducciones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/TipoDeduccion.cs
+++ Models/TipoDeduccion.cs
@@ -34,18 +34,33 @@ namespace HRM_PLUS_PROJECT.Models
         public bool IsTodoEmpleado { get; set; }
 
         [Display(Name = "Mínimo de rango")]
-        [Required(ErrorMessage = "Debe ingresar un mínimo de rango")]
         [Range(1, 9999999, ErrorMessage = "Ingresar un monto entre 1 y 9999999")]
         public decimal? MinimoRango { get; set; }
 
         [Display(Name = "Máximo de rango")]
-        [Required(ErrorMessage = "Debe ingresar un máximo de rango")]
         [Range(1, 9999999, ErrorMessage = "Ingresar un monto entre 1 y 9999999")]
+        [Cantidad1NoMayorQueCantidad2]
         public decimal? MaximoRango { get; set; }
 
         [Display(Name = "Estado")]
         public bool IsActivo { get; set; }
 
         public virtual ICollection<Nomina> Nominas { get; set; }
+
+        //Para validar que mínimo rango no sea mayor a máximo rango
+        public class Cantidad1NoMayorQueCantidad2Attribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                var model = (TipoDeduccion)validationContext.ObjectInstance;
+
+                if (model.MinimoRango > model.MaximoRango)
+                {
+                    return new ValidationResult("El salario mínimo no puede ser mayor al salario máximo");
+                }
+
+                return ValidationResult.Success;
+            }
+        }
     }
 }
